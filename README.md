f87112bc8f7d34d95153e0dc9a9fde7cb2d60ff5
Benry Yip <me@benryyip.com>
2018-01-06 16:02:52 +0700

    Add notes on page layout to README. Get books metadata to load pages for scraping.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # thsv112kindle
 Grab THSV11 text for personal use on a Kindle.
+
+## Dev Notes
+
+http://m.bibles.org/tha-THSV11/Gen/1
+http://m.bibles.org/tha-THSV11/[BookAbbr]/[Ch]
+
+```html
+div#passage
+  div#container
+  	div#main
+  	  div.passagedisplay
+  	  	div#[BookAbbr].[Ch3]
+  	  		h3.s SectionTitle
+  	  			LOOP: p.verse-block (a block of verses)
+  	  				span.v[Book#0]_[Ch#]_[V#] (This can be split over multiple p.verse-block's, like in the Psalms. Need to be combined.)
+  	  					sup#[BookAbbr].[Ch#].[V#] Verse #
+  	  					VerseText
+  	  					a.notelink (This can be deleted.)
+```
---
+++ extract_books_data.rb
@@ -0,0 +1,21 @@
+# Extracts Bible book name, abbreviation and number of chapters from
+# http://m.bibles.org/tha-THSV11/ navigation in order to go to proper pages for
+# scraping chapters.
+require 'json'
+require 'nokogiri'
+
+doc = File.open("books-nav.html") { |f| Nokogiri::XML(f) }
+doc.encoding = 'UTF-8'
+
+bible_books = []
+
+lis = doc.css("ul.book > li")
+lis.each do |li|
+	book_abbr = li['id']
+	chapter_name = li.css("a[data-ajax='false']")[0].text
+	num_chapters = li.css("ul.chapter > li > a:not([class='book-intro'])").count
+	#puts "#{book_abbr} : #{chapter_name} - (#{num_chapters})"
+	bible_books << {:abbr => book_abbr, :name => chapter_name, :num_chapters => num_chapters}
+end
+
+puts bible_books.to_json
