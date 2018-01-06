86d97605d6d2cf61a3309c4e174866071003d29c
Benry Yip <me@benryyip.com>
2018-01-06 19:22:41 +0700

    Add raw chapter html.

--- download_chapters.rb
+++ download_chapters.rb
@@ -6,25 +6,22 @@ require 'open-uri'
 
 root_uri = Addressable::URI.parse("http://m.bibles.org/")
 
-#filename = "pages/#{book_num}-#{book.gsub(/ /,'-')}-#{ch_num}.html" # 01-มัทธิว-1.html
-#print "Writing #{filename}..."
-#File.write(filename, passage)
-#puts " done."
-
-# {:abbr, :name, :num_chapters}
+# books => [{:abbr, :name, :num_chapters}]
 books = JSON.parse(File.read("books.json"), {:symbolize_names => true})
 books.each_index do |i|
 	book = books[i]
 	for ch in 1..book[:num_chapters] do
 		print "Processing #{book[:abbr]} #{ch}... "
+
+		# Grab page.
 		chapter_uri = Addressable::URI.parse("/tha-THSV11/#{book[:abbr]}/#{ch}")
 		doc = Nokogiri::HTML(open(Addressable::URI.join(root_uri, chapter_uri).normalize))
 
-		# Write to: raw/01-Gen-001.html
-		filename = "raw/#{i.to_s.rjust(2, '0')}-#{book[:abbr]}-#{ch.to_s.rjust(3, '0')}.html"
+		# Write to raw/01-Gen-001.html
+		book_num = i+1
+		filename = "raw/#{book_num.to_s.rjust(2, '0')}-#{book[:abbr]}-#{ch.to_s.rjust(3, '0')}.html"
 		File.write(filename, doc)
+
 		puts "saved to #{filename}."
-		break
 	end
-	break
 end
