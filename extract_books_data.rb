1a067fbc03a5cbf5f6dc85d97e41c7dee92e786f
Benry Yip <me@benryyip.com>
2018-01-07 11:18:19 +0700

    Fix trailing space for book names.

--- extract_books_data.rb
+++ extract_books_data.rb
@@ -12,10 +12,10 @@ bible_books = []
 lis = doc.css("ul.book > li")
 lis.each do |li|
 	book_abbr = li['id']
-	chapter_name = li.css("a[data-ajax='false']")[0].text
+	book_name = li.css("a[data-ajax='false']")[0].text.strip
 	num_chapters = li.css("ul.chapter > li > a:not([class='book-intro'])").count
 	#puts "#{book_abbr} : #{chapter_name} - (#{num_chapters})"
-	bible_books << {:abbr => book_abbr, :name => chapter_name, :num_chapters => num_chapters}
+	bible_books << {:abbr => book_abbr, :name => book_name, :num_chapters => num_chapters}
 end
 
 puts bible_books.to_json
